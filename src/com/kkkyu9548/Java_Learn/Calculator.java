1beebd371e5778923b35a0fdef7027a888b94288
kkkyu9548 <kikiki9548@gmail.com>
2021-05-06 22:06:51 +0900

    Calculate 클래스 Instance.java 로 이동 (옮겨도 Caluator.java 똑같이 작동함)

--- src/com/kkkyu9548/Java_Learn/Calculator.java
+++ src/com/kkkyu9548/Java_Learn/Calculator.java
@@ -1,19 +1,19 @@
 package com.kkkyu9548.Java_Learn;
 
-class Calculate {   //1. 왜 클래스에 public이 붙었을 때 에러가 났는지
-    int left, right;
-
-    public void setOprand(int left, int right){
-        this.left = left;
-        this.right = right;
-    }
-    public void sum(){
-        System.out.println("두 수의 합은 "+(this.left+this.right));  //괄호
-    }
-    public void avg(){
-        System.out.println("두 수의 평균은 "+(this.left+this.right)/2);
-    }
-}
+//class Calculate {   //1. 왜 클래스에 public이 붙었을 때 에러가 났는지
+//    int left, right;
+//
+//    public void setOprand(int left, int right){
+//        this.left = left;
+//        this.right = right;
+//    }
+//    public void sum(){
+//        System.out.println("두 수의 합은 "+(this.left+this.right));  //괄호
+//    }
+//    public void avg(){
+//        System.out.println("두 수의 평균은 "+(this.left+this.right)/2);
+//    }
+//}
 public class Calculator {
 
     public static void main(String[] args) {
