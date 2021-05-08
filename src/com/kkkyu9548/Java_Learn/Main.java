3270ab651d45397aa8e69710d3ccb3e23af24235
kkkyu9548 <kikiki9548@gmail.com>
2021-05-08 22:39:44 +0900

    숫자와 연산 (1)

--- src/com/kkkyu9548/Java_Learn/Main.java
+++ src/com/kkkyu9548/Java_Learn/Main.java
@@ -7,9 +7,15 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 //        sout+enter
-        System.out.println();
+        System.out.println(6);//Number
+        System.out.println(6+6);
+        System.out.println("6"+"6");
+        System.out.println(6*6);
+//        System.out.println("6"*"6");
+        System.out.println("1111".length());    //(문자열 길이)->4
+//        System.out.println(1111.length());
 //        souf
-        System.out.printf("");
+        System.out.printf("six & 6");//String six & 6 문자열
 //        soutm
         System.out.println("Main.main");
 //        soutp
