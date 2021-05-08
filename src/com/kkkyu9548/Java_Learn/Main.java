d02c5da23e2ad2cd14d72d5512ad3608eb322263
kkkyu9548 <kikiki9548@gmail.com>
2021-05-08 22:32:53 +0900

    시스아웃 단축키 연습

--- src/com/kkkyu9548/Java_Learn/Main.java
+++ src/com/kkkyu9548/Java_Learn/Main.java
@@ -1,8 +1,20 @@
 package com.kkkyu9548.Java_Learn;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+//        sout+enter
+        System.out.println();
+//        souf
+        System.out.printf("");
+//        soutm
+        System.out.println("Main.main");
+//        soutp
+        System.out.println("args = " + Arrays.deepToString(args));
+//        soutv
+        System.out.println("args = " + args);
     }
 }
