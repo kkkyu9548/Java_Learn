acfda281315898a850062c519ef5b63f25ed4cf8
kkkyu9548 <kikiki9548@gmail.com>
2021-05-08 22:47:30 +0900

    숫자와 연산 (2)

--- src/com/kkkyu9548/Java_Learn/Main.java
+++ src/com/kkkyu9548/Java_Learn/Main.java
@@ -14,6 +14,17 @@ public class Main {
 //        System.out.println("6"*"6");
         System.out.println("1111".length());    //(문자열 길이)->4
 //        System.out.println(1111.length());
+
+        System.out.println(6 + 2);  //  8
+        System.out.println(6 - 2);  //  4
+        System.out.println(6 * 2);  //  12
+        System.out.println(6 / 2);  //  3
+
+//      Math 함수 & (.)관련 명령어
+        System.out.println(Math.PI);    //  3.141592653589793
+        System.out.println(Math.floor(Math.PI));    //  3.0
+        System.out.println(Math.ceil(Math.PI));     //  4.0
+
 //        souf
         System.out.printf("six & 6");//String six & 6 문자열
 //        soutm
