790e3bb73b5cb9135c7ddda60a26053f7ed10b9e
kkkyu9548 <kikiki9548@gmail.com>
2021-05-14 11:03:23 +0900

    클래스 맴버, 인스턴스 맴버 (3/4) : 클래스 메소드

---
+++ src/com/kkkyu9548/Java_Learn/Calculator3.java
@@ -0,0 +1,21 @@
+package com.kkkyu9548.Java_Learn;
+
+public class Calculator3 {
+    public static void sum(int left, int right){
+        System.out.println(left+right);
+    }
+    public static void avg(int left,int right){
+        System.out.println((left+right)/2);
+    }
+}
+public class CalculatorDemo3 {
+    public static void main(String[] args) {
+        Calculator3.sum(10,20);
+        Calculator3.avg(10,20);
+
+        Calculator3.sum(20,40);
+        Calculator3.avg(20,40);
+
+
+    }
+}
