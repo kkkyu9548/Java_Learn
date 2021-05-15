5da0594e0b33fab10f89de78b50a03e71f0a5268
kkkyu9548 <kikiki9548@gmail.com>
2021-05-15 10:19:50 +0900

    유효범위(주석추가)

--- src/com/kkkyu9548/Java_Learn/ScopeDemo.java
+++ src/com/kkkyu9548/Java_Learn/ScopeDemo.java
@@ -8,7 +8,7 @@ public class ScopeDemo {
 
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            a();
+            a();//i값은 a() 메소드 바깥에서는 값이 변경되지 않기 때문에 무한반복 되지 않는다.***************
             System.out.println(i);
         }
     }
