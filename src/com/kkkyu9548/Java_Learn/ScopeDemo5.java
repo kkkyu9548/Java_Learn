34649ecb74f1a6e1767b1796c6b9ec8c338735c5
kkkyu9548 <kikiki9548@gmail.com>
2021-05-15 10:33:54 +0900

    유효범위5

---
+++ src/com/kkkyu9548/Java_Learn/ScopeDemo5.java
@@ -0,0 +1,11 @@
+package com.kkkyu9548.Java_Learn;
+
+public class ScopeDemo5 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+        // System.out.println(i);
+    }
+
+}
