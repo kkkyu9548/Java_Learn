c96f2960490d43398610a8eaa4dca4265acad98f
kkkyu9548 <kikiki9548@gmail.com>
2021-05-17 16:01:34 +0900

    상속과 생성자-2

--- src/com/kkkyu9548/Java_Learn/ConstructorDemo.java
+++ src/com/kkkyu9548/Java_Learn/ConstructorDemo.java
@@ -1,7 +1,9 @@
 package com.kkkyu9548.Java_Learn;
 
 public class ConstructorDemo {
+    public ConstructorDemo(int param1) {}
     public static void main(String[] args) {
+
         ConstructorDemo c = new ConstructorDemo();
     }
 }
