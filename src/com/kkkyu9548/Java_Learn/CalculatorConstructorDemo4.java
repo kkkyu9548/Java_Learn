02f43074faa4df2901adc225b5d380736cd291db
kkkyu9548 <kikiki9548@gmail.com>
2021-05-17 16:16:36 +0900

    상속과 생성자-6

--- src/com/kkkyu9548/Java_Learn/CalculatorConstructorDemo4.java
+++ src/com/kkkyu9548/Java_Learn/CalculatorConstructorDemo4.java
@@ -1,32 +1,32 @@
 package com.kkkyu9548.Java_Learn;
 
-class Calculator {
-    int left, right;
-
-    public void setOprands(int left, int right) {
-        this.left = left;
-        this.right = right;
-    }
-
-    public void sum() {
-        System.out.println(this.left + this.right);
-    }
-
-    public void avg() {
-        System.out.println((this.left + this.right) / 2);
-    }
-}
-
-class SubstractionableCalculator extends Calculator {
-    public SubstractionableCalculator(int left, int right) {
-        this.left = left;
-        this.right = right;
-    }
-
-    public void substract() {
-        System.out.println(this.left - this.right);
-    }
-}
+//class Calculator {
+//    int left, right;
+//
+//    public void setOprands(int left, int right) {
+//        this.left = left;
+//        this.right = right;
+//    }
+//
+//    public void sum() {
+//        System.out.println(this.left + this.right);
+//    }
+//
+//    public void avg() {
+//        System.out.println((this.left + this.right) / 2);
+//    }
+//}
+//
+//class SubstractionableCalculator extends Calculator {
+//    public SubstractionableCalculator(int left, int right) {
+//        this.left = left;
+//        this.right = right;
+//    }
+//
+//    public void substract() {
+//        System.out.println(this.left - this.right);
+//    }
+//}
 
 public class CalculatorConstructorDemo4 {
     public static void main(String[] args) {
--- src/com/kkkyu9548/Java_Learn/CalculatorConstructorDemo5.java
+++ src/com/kkkyu9548/Java_Learn/CalculatorConstructorDemo5.java
@@ -3,6 +3,8 @@ package com.kkkyu9548.Java_Learn;
 class Calculator {
     int left, right;
 
+    public Calculator(){}
+
     public Calculator(int left, int right){
         this.left = left;
         this.right = right;
@@ -21,11 +23,9 @@ class Calculator {
         System.out.println((this.left + this.right) / 2);
     }
 }
-
 class SubstractionableCalculator extends Calculator {
     public SubstractionableCalculator(int left, int right) {
-        this.left = left;
-        this.right = right;
+        super(left, right);
     }
 
     public void substract() {
