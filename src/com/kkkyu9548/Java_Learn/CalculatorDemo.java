de4b8c4aa55b5bc91ee10b8e76a07bf7440bde27
kkkyu9548 <kikiki9548@gmail.com>
2021-05-21 11:00:43 +0900

    오버로딩-1

--- src/com/kkkyu9548/Java_Learn/CalculatorDemo.java
+++ src/com/kkkyu9548/Java_Learn/CalculatorDemo.java
@@ -1,43 +1,43 @@
 package com.kkkyu9548.Java_Learn;
 
-class Calculator {
+class Calculator{
     int left, right;
+    int third = 0;
 
-    public void setOprands(int left, int right) {
+    public void setOprands(int left, int right){
+        System.out.println("setOprands(int left, int right)");
         this.left = left;
         this.right = right;
     }
 
-    public void sum() {
-        System.out.println(this.left + this.right);
+    public void setOprands(int left, int right, int third){
+        System.out.println("setOprands(int left, int right, int third)");
+        this.left = left;
+        this.right = right;
+        this.third = third;
     }
 
-    public int avg() {
-        return ((this.left + this.right) / 2);
-    }
-}
-
-class SubstractionableCalculator extends Calculator {
-
-    public void sum() {
-        System.out.println("실행 결과는 " +(this.left + this.right)+"입니다.");
+    public void sum(){
+        System.out.println(this.left+this.right+this.third);
     }
 
-    public int avg() {
-        return super.avg();
-    }
-
-    public void substract() {
-        System.out.println(this.left - this.right);
+    public void avg(){
+        System.out.println((this.left+this.right+this.third)/3);
     }
 }
 
 public class CalculatorDemo {
+
     public static void main(String[] args) {
-        SubstractionableCalculator c1 = new SubstractionableCalculator();
+
+        Calculator c1 = new Calculator();
         c1.setOprands(10, 20);
         c1.sum();
-        System.out.println("실행 결과는" + c1.avg());
-        c1.substract();
+        c1.avg();
+        c1.setOprands(10, 20, 30);
+        c1.sum();
+        c1.avg();
+
     }
+
 }
