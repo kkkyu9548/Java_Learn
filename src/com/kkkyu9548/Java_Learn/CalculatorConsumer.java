638fded6c07b6180adbf1c608f1d7f2ace953f86
kkkyu9548 <kikiki9548@gmail.com>
2021-05-23 21:04:54 +0900

    interface(4)

--- src/com/kkkyu9548/Java_Learn/CalculatorConsumer.java
+++ src/com/kkkyu9548/Java_Learn/CalculatorConsumer.java
@@ -1,31 +1,18 @@
 package com.kkkyu9548.Java_Learn;
 
-interface I1{
-    public void x();
+class CalculatorDummy{
+    public void setOprands(int first, int second, int third){}
+    public int sum(){
+        return 60;
+    }
+    public int avg(){
+        return 20;
+    }
 }
-
-interface I2{
-    public void z();
-}
-
-class A implements I1, I2{
-    public void x(){}
-    public void z(){}
-}
-
-interface I3{
-    public void x();
-}
-
-interface I4 extends I3{
-    public void z();
-}
-
-class B implements I4{
-    public void x(){}
-    public void z(){}
-}
-
-interface I5{
-    private void x();
+public class CalculatorConsumer {
+    public static void main(String[] args){
+        CalculatorDummy c = new CalculatorDummy();
+        c.setOprands(10,20,30);
+        System.out.println(c.sum()+c.avg());
+    }
 }
