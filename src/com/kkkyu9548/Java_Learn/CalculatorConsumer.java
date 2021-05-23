bc7f2d7fbd797fabcf0557627a5ce9cb94d1c6f6
kkkyu9548 <kikiki9548@gmail.com>
2021-05-23 21:06:32 +0900

    interface(5)

--- src/com/kkkyu9548/Java_Learn/CalculatorConsumer.java
+++ src/com/kkkyu9548/Java_Learn/CalculatorConsumer.java
@@ -1,7 +1,28 @@
 package com.kkkyu9548.Java_Learn;
 
-class CalculatorDummy{
-    public void setOprands(int first, int second, int third){}
+class Calculator {
+    int left, right;
+    public void setOprands(int left, int right) {
+        this.left = left;
+        this.right = right;
+    }
+    public void sum() {
+        System.out.println(this.left + this.right);
+    }
+    public void avg() {
+        System.out.println((this.left + this.right) / 2);
+    }
+}
+
+interface Calculatable {
+    public void setOprands(int first, int second, int third) ;
+    public int sum();
+    public int avg();
+}
+
+class CalculatorDummy implements Calculatable{
+    public void setOprands(int first, int second, int third){
+    }
     public int sum(){
         return 60;
     }
@@ -10,9 +31,9 @@ class CalculatorDummy{
     }
 }
 public class CalculatorConsumer {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         CalculatorDummy c = new CalculatorDummy();
-        c.setOprands(10,20,30);
+        c.setOprands(10, 20, 30);
         System.out.println(c.sum()+c.avg());
     }
 }
