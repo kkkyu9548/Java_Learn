26764e6c369a5dd0df338c9ed93158d433b0359d
kkkyu9548 <kikiki9548@gmail.com>
2021-05-24 21:03:26 +0900

    다형성-5

--- src/com/kkkyu9548/Java_Learn/PolymorphismDemo1.java
+++ src/com/kkkyu9548/Java_Learn/PolymorphismDemo1.java
@@ -1,15 +1,15 @@
 package com.kkkyu9548.Java_Learn;
 
 class A{
-    public String x(){return "x";}
+    public String x(){return "A.x";}
 }
 class B extends A{
+    public String x(){return "B.x";}
     public String y(){return "y";}
 }
 public class PolymorphismDemo1 {
     public static void main(String[] args) {
         A obj = new B();
-        obj.x();
-        obj.y();
+        System.out.println(obj.x());
     }
 }
