4d97047c6995e69060b1dcb2ae9c06a1b5255ed0
kkkyu9548 <kikiki9548@gmail.com>
2021-05-26 22:01:19 +0900

    PolymorphismDemo2.java 제거

--- src/com/kkkyu9548/Java_Learn/PolymorphismDemo2.java
+++
@@ -1,9 +0,0 @@
-package com.kkkyu9548.Java_Learn;
-
-interface I{}
-class C implements I{}
-public class PolymorphismDemo2 {
-    public static void main(String[] args) {
-        I obj = new C();
-    }
-}
