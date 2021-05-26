70d704dea26e8fbc779c51c608729d56dc3b0024
kkkyu9548 <kikiki9548@gmail.com>
2021-05-26 22:04:00 +0900

    예외-2

--- src/com/kkkyu9548/Java_Learn/CalculatorDemo.java
+++ src/com/kkkyu9548/Java_Learn/CalculatorDemo.java
@@ -7,9 +7,13 @@ class Calculator{
         this.right = right;
     }
     public void divide(){
-        System.out.print("계산결과는 ");
-        System.out.print(this.left/this.right);
-        System.out.print(" 입니다.");
+        try {
+            System.out.print("계산결과는 ");
+            System.out.print(this.left/this.right);
+            System.out.print(" 입니다.");
+        } catch(Exception e){
+            System.out.println("오류가 발생했습니다 : "+e.getMessage());
+        }
     }
 }
 public class CalculatorDemo {
@@ -17,5 +21,9 @@ public class CalculatorDemo {
         Calculator c1 = new Calculator();
         c1.setOprands(10, 0);
         c1.divide();
+
+        Calculator c2 = new Calculator();
+        c2.setOprands(10, 5);
+        c2.divide();
     }
 }
