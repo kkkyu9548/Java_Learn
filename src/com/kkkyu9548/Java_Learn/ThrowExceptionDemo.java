5c9fb47cde4bb0c687a24002b2d96a7ddc30424d
kkkyu9548 <kikiki9548@gmail.com>
2021-05-28 21:25:03 +0900

    예외-19

---
+++ src/com/kkkyu9548/Java_Learn/ThrowExceptionDemo.java
@@ -0,0 +1,18 @@
+package com.kkkyu9548.Java_Learn;
+
+class B{
+    void run(){
+    }
+}
+class C{
+    void run(){
+        B b = new B();
+        b.run();
+    }
+}
+public class ThrowExceptionDemo {
+    public static void main(String[] args) {
+        C c = new C();
+        c.run();
+    }
+}
