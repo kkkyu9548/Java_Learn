e90f207bc616df06ce963bed3e8a6b76be95f1f0
kkkyu9548 <kikiki9548@gmail.com>
2021-05-28 21:23:19 +0900

    예외-15

--- src/com/kkkyu9548/Java_Learn/CheckedExceptionDemo.java
+++ src/com/kkkyu9548/Java_Learn/CheckedExceptionDemo.java
@@ -3,7 +3,11 @@ package com.kkkyu9548.Java_Learn;
 import java.io.*;
 public class CheckedExceptionDemo {
     public static void main(String[] args) {
-        BufferedReader bReader = new BufferedReader(new FileReader("out.txt"));
+        try {
+            BufferedReader bReader = new BufferedReader(new FileReader("out.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         String input = bReader.readLine();
         System.out.println(input);
     }
