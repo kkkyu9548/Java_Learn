a230ac165dccd6ff7faa77b4a583eecba9d85d11
kkkyu9548 <kikiki9548@gmail.com>
2021-05-28 21:35:56 +0900

    예외-26

--- src/com/kkkyu9548/Java_Learn/ThrowExceptionDemo.java
+++ src/com/kkkyu9548/Java_Learn/ThrowExceptionDemo.java
@@ -11,20 +11,20 @@ class B{
     }
 }
 class C{
-    void run(){
+    void run() throws IOException, FileNotFoundException{
         B b = new B();
-        try {
-            b.run();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        b.run();
     }
 }
 public class ThrowExceptionDemo {
     public static void main(String[] args) {
         C c = new C();
-        c.run();
+        try {
+            c.run();
+        } catch (FileNotFoundException e) {
+            System.out.println("out.txt 파일은 설정 파일 입니다. 이 파일이 프로잭트 루트 디렉토리에 존재해야 합니다.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
