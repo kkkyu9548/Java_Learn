feda8d5e1bb3e9ae6a116eac36cbfb25b401f62c
kkkyu9548 <kikiki9548@gmail.com>
2021-05-29 19:49:14 +0900

    예외-39

--- src/com/kkkyu9548/Java_Learn/CalculatorDemo.java
+++ src/com/kkkyu9548/Java_Learn/CalculatorDemo.java
@@ -1,6 +1,13 @@
 package com.kkkyu9548.Java_Learn;
-import java.io.IOException;
 
+class DivideException extends RuntimeException {
+    DivideException(){
+        super();
+    }
+    DivideException(String message){
+        super(message);
+    }
+}
 class Calculator{
     int left, right;
     public void setOprands(int left, int right){
@@ -9,18 +16,9 @@ class Calculator{
     }
     public void divide(){
         if(this.right == 0){
-            throw new ArithmeticException("0으로 나누는 것은 허용되지 않습니다.");
+            throw new DivideException("0으로 나누는 것은 허용되지 않습니다.");
         }
-        try {
-            System.out.print("계산결과는 ");
-            System.out.print(this.left/this.right);
-            System.out.print(" 입니다.");
-        } catch(Exception e){
-            System.out.println("\n\ne.getMessage()\n"+e.getMessage());
-            System.out.println("\n\ne.toString()\n"+e.toString());
-            System.out.println("\n\ne.printStackTrace()");
-            e.printStackTrace();
-        }
+        System.out.print(this.left/this.right);
     }
 }
 public class CalculatorDemo {
@@ -28,22 +26,5 @@ public class CalculatorDemo {
         Calculator c1 = new Calculator();
         c1.setOprands(10, 0);
         c1.divide();
-    }
-}
-
-
-class E{
-    void throwArithmeticException(){
-        throw new ArithmeticException();
-    }
-    void throwIOException1(){
-        try {
-            throw new IOException();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    void throwIOException2() throws IOException{
-        throw new IOException();
     }
 }
