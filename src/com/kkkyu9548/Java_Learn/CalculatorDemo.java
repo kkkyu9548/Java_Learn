e493b976e7a729cdede48e48fcb50c47fff4fc5f
kkkyu9548 <kikiki9548@gmail.com>
2021-05-29 19:47:03 +0900

    예외-37

--- src/com/kkkyu9548/Java_Learn/CalculatorDemo.java
+++ src/com/kkkyu9548/Java_Learn/CalculatorDemo.java
@@ -36,7 +36,14 @@ class E{
     void throwArithmeticException(){
         throw new ArithmeticException();
     }
-    void throwIOException(){
+    void throwIOException1(){
+        try {
+            throw new IOException();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    void throwIOException2() throws IOException{
         throw new IOException();
     }
 }
