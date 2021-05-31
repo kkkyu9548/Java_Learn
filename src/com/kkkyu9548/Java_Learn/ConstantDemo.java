381c4e494f297f56ada726fafbc9e704bf37e250
kkkyu9548 <kikiki9548@gmail.com>
2021-05-31 21:24:31 +0900

    enum-3 (상수 복습)

--- src/com/kkkyu9548/Java_Learn/ConstantDemo.java
+++ src/com/kkkyu9548/Java_Learn/ConstantDemo.java
@@ -1,24 +1,21 @@
 package com.kkkyu9548.Java_Learn;
 
 public class ConstantDemo {
+    private final static int APPLE = 1;
+    private final static int PEACH = 2;
+    private final static int BANANA = 3;
     public static void main(String[] args) {
-        /*
-         * 1. 사과
-         * 2. 복숭아
-         * 3. 바나나
-         */
-        int type = 1;
+        int type = APPLE;
         switch(type){
-            case 1:
-                System.out.println(57);
+            case APPLE:
+                System.out.println(57+" kcal");
                 break;
-            case 2:
-                System.out.println(34);
+            case PEACH:
+                System.out.println(34+" kcal");
                 break;
-            case 3:
-                System.out.println(93);
+            case BANANA:
+                System.out.println(93+" kcal");
                 break;
         }
     }
-
 }
