506b2a9805a33eaa02c4b57b54206ae1c78f65dd
kkkyu9548 <kikiki9548@gmail.com>
2021-05-31 21:20:12 +0900

    오브젝트-8

--- src/com/kkkyu9548/Java_Learn/ObjectDemo.java
+++ src/com/kkkyu9548/Java_Learn/ObjectDemo.java
@@ -1,13 +1,12 @@
 package com.kkkyu9548.Java_Learn;
 
-class Student{
+class Student implements Cloneable{
     String name;
     Student(String name){
         this.name = name;
     }
-    public boolean equals(Object obj) {
-        Student _obj = (Student)obj;
-        return name == _obj.name;
+    protected Object clone() throws CloneNotSupportedException{
+        return super.clone();
     }
 }
 
@@ -15,10 +14,13 @@ class ObjectDemo {
 
     public static void main(String[] args) {
         Student s1 = new Student("egoing");
-        Student s2 = new Student("egoing");
-        System.out.println(s1 == s2);
-        System.out.println(s1.equals(s2));
-
+        try {
+            Student s2 = (Student)s1.clone();
+            System.out.println(s1.name);
+            System.out.println(s2.name);
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
