9e424478688f90a191426baed9fced2e4cf098c3
kkkyu9548 <kikiki9548@gmail.com>
2021-06-01 20:48:34 +0900

    enum-17 (상수 복습)

--- src/com/kkkyu9548/Java_Learn/ConstantDemo.java
+++ src/com/kkkyu9548/Java_Learn/ConstantDemo.java
@@ -1,9 +1,11 @@
 package com.kkkyu9548.Java_Learn;
 
 enum Fruit{
-    APPLE, PEACH, BANANA;
-    Fruit(){
+    APPLE("red"), PEACH("pink"), BANANA("yellow");
+    public String color;
+    Fruit(String color){
         System.out.println("Call Constructor "+this);
+        this.color = color;
     }
 }
 
@@ -14,7 +16,6 @@ enum Company{
 public class ConstantDemo {
 
     public static void main(String[] args) {
-
         /*
         if(Fruit.APPLE == Company.APPLE){
             System.out.println("과일 애플과 회사 애플이 같다.");
@@ -23,13 +24,13 @@ public class ConstantDemo {
         Fruit type = Fruit.APPLE;
         switch(type){
             case APPLE:
-                System.out.println(57+" kcal");
+                System.out.println(57+" kcal, "+Fruit.APPLE.color);
                 break;
             case PEACH:
-                System.out.println(34+" kcal");
+                System.out.println(34+" kcal"+Fruit.PEACH.color);
                 break;
             case BANANA:
-                System.out.println(93+" kcal");
+                System.out.println(93+" kcal"+Fruit.BANANA.color);
                 break;
         }
     }
