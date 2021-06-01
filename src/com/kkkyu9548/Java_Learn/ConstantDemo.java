cc5191883c479f5c4b275a0dc3692355f7ef39a1
kkkyu9548 <kikiki9548@gmail.com>
2021-06-01 20:51:13 +0900

    enum-21 (상수 복습)

--- src/com/kkkyu9548/Java_Learn/ConstantDemo.java
+++ src/com/kkkyu9548/Java_Learn/ConstantDemo.java
@@ -19,17 +19,8 @@ enum Company{
 public class ConstantDemo {
 
     public static void main(String[] args) {
-        Fruit type = Fruit.APPLE;
-        switch(type){
-            case APPLE:
-                System.out.println(57+" kcal, "+Fruit.APPLE.getColor());
-                break;
-            case PEACH:
-                System.out.println(34+" kcal"+Fruit.PEACH.getColor());
-                break;
-            case BANANA:
-                System.out.println(93+" kcal"+Fruit.BANANA.getColor());
-                break;
+        for(Fruit f : Fruit.values()){
+            System.out.println(f+", "+f.getColor());
         }
     }
 }
