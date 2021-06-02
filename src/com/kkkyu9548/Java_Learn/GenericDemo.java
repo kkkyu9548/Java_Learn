ae1b29b3867e1d305fcd8791e0c9c67dde4c2fb5
kkkyu9548 <kikiki9548@gmail.com>
2021-06-02 21:37:57 +0900

    제네릭-3

--- src/com/kkkyu9548/Java_Learn/GenericDemo.java
+++ src/com/kkkyu9548/Java_Learn/GenericDemo.java
@@ -1,14 +1,28 @@
 package com.kkkyu9548.Java_Learn;
 
-class Person<T>{
-    public T info;
+class StudentInfo{
+    public int grade;
+    StudentInfo(int grade){ this.grade = grade; }
 }
-
+class StudentPerson{
+    public StudentInfo info;
+    StudentPerson(StudentInfo info){ this.info = info; }
+}
+class EmployeeInfo{
+    public int rank;
+    EmployeeInfo(int rank){ this.rank = rank; }
+}
+class EmployeePerson{
+    public EmployeeInfo info;
+    EmployeePerson(EmployeeInfo info){ this.info = info; }
+}
 public class GenericDemo {
-
     public static void main(String[] args) {
-        Person<String> p1 = new Person<String>();
-        Person<StringBuilder> p2 = new Person<StringBuilder>();
+        StudentInfo si = new StudentInfo(2);
+        StudentPerson sp = new StudentPerson(si);
+        System.out.println(sp.info.grade); // 2
+        EmployeeInfo ei = new EmployeeInfo(1);
+        EmployeePerson ep = new EmployeePerson(ei);
+        System.out.println(ep.info.rank); // 1
     }
-
 }
