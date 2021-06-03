211225351e6698832fb1c26d71b65f5f131df757
kkkyu9548 <kikiki9548@gmail.com>
2021-06-03 21:04:43 +0900

    제네릭-13

--- src/com/kkkyu9548/Java_Learn/GenericDemo.java
+++ src/com/kkkyu9548/Java_Learn/GenericDemo.java
@@ -14,6 +14,28 @@ class Person<T, S>{
 }
 public class GenericDemo {
     public static void main(String[] args) {
-        Person<EmployeeInfo, int> p1 = new Person<EmployeeInfo, int>(new EmployeeInfo(1), 1);
+        EmployeeInfo e = new EmployeeInfo(1);
+        Integer i = new Integer(10);
+        Person<EmployeeInfo, Integer> p1 = new Person<EmployeeInfo, Integer>(e, i);
+        System.out.println(p1.id.intValue());
+    }
+}class EmployeeInfo{
+    public int rank;
+    EmployeeInfo(int rank){ this.rank = rank; }
+}
+class Person<T, S>{
+    public T info;
+    public S id;
+    Person(T info, S id){
+        this.info = info;
+        this.id = id;
+    }
+}
+public class GenericDemo {
+    public static void main(String[] args) {
+        EmployeeInfo e = new EmployeeInfo(1);
+        Integer i = new Integer(10);
+        Person<EmployeeInfo, Integer> p1 = new Person<EmployeeInfo, Integer>(e, i);
+        System.out.println(p1.id.intValue());
     }
 }
