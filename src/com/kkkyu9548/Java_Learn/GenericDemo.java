72891390cec04bbc17f78099076d79f580d410c2
kkkyu9548 <kikiki9548@gmail.com>
2021-06-03 21:03:41 +0900

    제네릭-11

--- src/com/kkkyu9548/Java_Learn/GenericDemo.java
+++ src/com/kkkyu9548/Java_Learn/GenericDemo.java
@@ -1,25 +1,19 @@
 package com.kkkyu9548.Java_Learn;
 
-class StudentInfo{
-    public int grade;
-    StudentInfo(int grade){ this.grade = grade; }
-}
 class EmployeeInfo{
     public int rank;
     EmployeeInfo(int rank){ this.rank = rank; }
 }
-class Person<T>{
+class Person<T, S>{
     public T info;
-    Person(T info){ this.info = info; }
+    public S id;
+    Person(T info, S id){
+        this.info = info;
+        this.id = id;
+    }
 }
 public class GenericDemo {
     public static void main(String[] args) {
-        Person<EmployeeInfo> p1 = new Person<EmployeeInfo>(new EmployeeInfo(1));
-        EmployeeInfo ei1 = p1.info;
-        System.out.println(ei1.rank); // 성공
-
-        Person<String> p2 = new Person<String>("부장");
-        String ei2 = p2.info;
-        System.out.println(ei2.rank); // 컴파일 실패
+        Person<EmployeeInfo, int> p1 = new Person<EmployeeInfo, int>(new EmployeeInfo(1), 1);
     }
 }
